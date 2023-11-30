a4eaccc36190a932dbe939307868f71afb51c315
ViniciusAndBar <vini.zeus2002@gmail.com>
2023-11-30 10:03:22 -0300

    Ajustes

--- lib/carrinho_compra.dart
+++ lib/carrinho_compra.dart
@@ -3,13 +3,12 @@ import 'package:flutter_sqlite/listagem_produtos.dart';
 import 'package:flutter_sqlite/login.dart';
 import 'package:flutter_sqlite/model/produto.dart';
 
+
 class CarrinhoCompra extends StatefulWidget {
   final List<int> prodSelecionados;
   final List<Produto> produtos;
 
-  const CarrinhoCompra(
-      {Key? key, required this.produtos, required this.prodSelecionados})
-      : super(key: key);
+  const CarrinhoCompra({Key? key, required this.produtos, required this.prodSelecionados}) : super(key: key);
 
   @override
   CarrinhoCompraState createState() => CarrinhoCompraState();
@@ -66,99 +65,101 @@ class CarrinhoCompraState extends State<CarrinhoCompra> {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
+          icon: Icon(Icons.arrow_back),
           onPressed: () {
             Navigator.of(context).pop();
             Navigator.push(
               context,
-              MaterialPageRoute(
-                builder: (context) =>
-                    ListagemProdutos(selecionados: widget.prodSelecionados),
-              ),
+              MaterialPageRoute(builder: (context) => ListagemProdutos(selecionados: widget.prodSelecionados)),
             );
+            ;
           },
         ),
         title: const Text("Carrinho"),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
+      body: Align(
+        alignment: Alignment.topCenter,
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: [
-            const SizedBox(height: 20.0),
-            Text(
-              'Quantidade de produtos ${calcularQuantidadeTotal()}',
-              style: const TextStyle(
-                  fontSize: 25.0, color: Color.fromARGB(255, 3, 46, 81)),
-            ),
-            Text(
-              'Valor total: ${calcularValorTotal()[0]}',
-              style: const TextStyle(
-                  fontSize: 25.0, color: Color.fromARGB(255, 3, 46, 81)),
-            ),
+            const SizedBox(height: 50.0),
+            Text('Quantidade de produtos ${calcularQuantidadeTotal()}',
+                style: TextStyle(fontSize: 25.0, color: Color.fromARGB(255, 3, 46, 81))),
+            Text('Valor total: ${calcularValorTotal()[0]}',
+                style: TextStyle(fontSize: 25.0, color: Color.fromARGB(255, 3, 46, 81))),
             Text(desconto ? 'Desconto: 5%' : 'Desconto: 0%'),
-            desconto
-                ? Text("Desconto: ${calcularValorTotal()[1]}")
-                : Container(),
-            const SizedBox(height: 20.0),
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: _produtos.length,
-              itemBuilder: (BuildContext context, int index) {
-                final item = _produtos[index];
-                return Card(
-                  elevation: 4.0,
-                  margin: const EdgeInsets.symmetric(vertical: 10.0),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              'Cod#${item.id}',
-                              style: const TextStyle(
-                                  fontSize: 18.0, fontWeight: FontWeight.bold),
-                            ),
-                            Text(
-                              '${item.nome}',
-                              style: const TextStyle(
-                                  fontSize: 18.0, fontWeight: FontWeight.bold),
-                            ),
-                            Text(
-                              'Preço: ${item.preco}',
-                              style: const TextStyle(
-                                  fontSize: 18.0, fontWeight: FontWeight.bold),
-                            ),
-                          ],
+            desconto ? Text("Desconto: ${calcularValorTotal()[1]}") : Text(""),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1.0),
+                ),
+                child: ListView.builder(
+                  itemCount: _produtos.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final item = _produtos[index];
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(8.0),
                         ),
-                        const SizedBox(height: 8.0),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            const Text('Quantidade'),
-                            const SizedBox(width: 10),
-                            Expanded(
-                              child: TextFormField(
-                                keyboardType: TextInputType.number,
-                                onChanged: (value) {
-                                  setState(() {
-                                    quantidadesList[index] = int.parse(value);
-                                  });
-                                },
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Text('Cod#${item.id}',
+                                      style: const TextStyle(
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.bold)),
+                                  const SizedBox(height: 8.0),
+                                  Text('${item.nome}',
+                                      style: const TextStyle(
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.bold)),
+                                  const SizedBox(height: 8.0),
+                                  const Text('Preço:'),
+                                  Text('${item.preco}',
+                                      style: const TextStyle(
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.bold)),
+                                ],
                               ),
-                            ),
-                          ],
+                              const SizedBox(height: 8.0),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  const Text('Quantidade'),
+                                  const SizedBox(width: 10),
+                                  Expanded(
+                                    child: TextFormField(
+                                      keyboardType: TextInputType.number,
+                                      onChanged: (value) {
+                                        setState(() {
+                                          quantidadesList[index] =
+                                              int.parse(value);
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
-                      ],
-                    ),
-                  ),
-                );
-              },
+                      ),
+                    );
+                  },
+                ),
+              ),
             ),
-            const SizedBox(height: 20.0),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -180,10 +181,8 @@ class CarrinhoCompraState extends State<CarrinhoCompra> {
                   onPressed: () {
                     ScaffoldMessenger.of(context).showSnackBar(
                       SnackBar(
-                        backgroundColor:
-                            const Color.fromARGB(255, 58, 172, 243),
-                        content: const Text('Pedido Enviado Com Sucesso'),
-                      ),
+                          backgroundColor: Color(Color.fromARGB(255, 58, 172, 243).value),
+                          content: Text('Pedido Enviado Com Sucesso')),
                     );
                   },
                   style: ElevatedButton.styleFrom(
@@ -193,7 +192,6 @@ class CarrinhoCompraState extends State<CarrinhoCompra> {
                 ),
               ],
             ),
-            const SizedBox(height: 20.0),
           ],
         ),
       ),
