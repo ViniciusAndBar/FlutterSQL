c7d6f5cab905fb40ba113454c1885e317c0e4c47
ViniciusAndBar <vini.zeus2002@gmail.com>
2023-11-30 10:46:25 -0300

    Adição da imagem

--- lib/database/db.dart
+++ lib/database/db.dart
@@ -23,9 +23,7 @@ class DB {
       path,
       version: 1,
       onCreate: (db, version) async {
-        await db.execute('''DROP TABLE usuarios''');
-        await db.execute('''DROP TABLE produtos''');
-        await db.execute('''DROP TABLE compras''');
+
         await db.execute(
           '''CREATE TABLE usuarios(
               id INTEGER PRIMARY KEY AUTOINCREMENT, 
@@ -56,6 +54,7 @@ class DB {
       },
     );
     List<Usuario> users = await getAllUsuarios();
+    print(users);
     if (users.isEmpty) {
       print("Executando inserts...");
       for (int i = 0; i < inserts.length; i++) {
@@ -80,9 +79,11 @@ class DB {
     });
   }
 
-  Future<List<Usuario>> getUserByEmailAndPassword(String email, String senha) async {
+  Future<List<Usuario>> getUserByEmailAndPassword(
+      String email, String senha) async {
     String query = "SELECT * FROM usuarios WHERE email = ? AND senha = ?";
-    final List<Map<String, dynamic>> maps = await _database.rawQuery(query, ['$email', '$senha']);
+    final List<Map<String, dynamic>> maps =
+        await _database.rawQuery(query, ['$email', '$senha']);
     if (maps.isNotEmpty) {
       return List.generate(maps.length, (i) {
         return Usuario(
@@ -91,7 +92,7 @@ class DB {
             email: maps[i]["email"],
             senha: maps[i]['senha']);
       });
-    }else{
+    } else {
       return [];
     }
   }
@@ -102,8 +103,9 @@ class DB {
     if (response.statusCode == 200) {
       final jsonResponse = json.decode(response.body);
       for (int i = 0; i < jsonResponse.length; i++) {
-        List<Produto> produtos = await getProductByName(jsonResponse[i]['nome']);
-        if (!produtos.isEmpty) {
+        List<Produto> produtos =
+            await getProductByName(jsonResponse[i]['nome']);
+        if (produtos.isEmpty) {
           String query =
               "INSERT INTO produtos(nome,descricao, preco, quantidade) VALUES('${jsonResponse[i]['nome']}','${jsonResponse[i]['descricao']}', ${jsonResponse[i]['preco']}, ${jsonResponse[i]['quantidade']})";
           await _database.execute(query);
